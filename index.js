f10d51352840354ae2ed7594f4d1b93a46498f67
Ekaterina Dubtsova <e.dubcova@profsoft.pro>
2022-12-20 10:47:34 +0100

    add 2 task

--- index.js
+++ index.js
@@ -16,12 +16,54 @@ Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
 
 const runningSum = function(nums) {
     for(let i = 1; i < nums.length; i++) {
-        debugger;
         nums[i] = nums[i - 1] + nums[i];
     }
-
     return nums;
 };
 
 console.log(runningSum([1,2,3,4]))
 console.log(runningSum([1,5,1,2]))
+
+
+/*
+1342. Number of Steps to Reduce a Number to Zero
+
+Given an integer num, return the number of steps to reduce it to zero.
+
+In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+
+Example 1:
+
+Input: num = 14
+Output: 6
+Explanation: 
+Step 1) 14 is even; divide by 2 and obtain 7. 
+Step 2) 7 is odd; subtract 1 and obtain 6.
+Step 3) 6 is even; divide by 2 and obtain 3. 
+Step 4) 3 is odd; subtract 1 and obtain 2. 
+Step 5) 2 is even; divide by 2 and obtain 1. 
+Step 6) 1 is odd; subtract 1 and obtain 0.
+*/
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+
+var numberOfSteps = function(num) {
+    let counter = 0;
+    while(num > 0) {
+        if(num % 2 == 0) {
+            num = num/2;
+        }
+        else {
+            num--;
+        }
+        counter++
+    }
+    return counter;
+}
+
+console.log(numberOfSteps(14)) // 6
+console.log(runningSum(8)) // 4
+console.log(runningSum(123)) // 12
