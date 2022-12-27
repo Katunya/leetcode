bcbf3853fe8a535b60159a24059512a9c6007387
Ekaterina Dubtsova <e.dubcova@profsoft.pro>
2022-12-27 17:53:07 +0100

    add task

--- index.js
+++ index.js
@@ -99,3 +99,43 @@ console.log(fizzBuzz(5)) // ["1","2","Fizz","4","Buzz"]
 console.log(fizzBuzz(15)) // Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
 
 
+
+/*
+1. Two Sum
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+*/
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ var twoSum = function(nums, target) {
+    let sum = 0;
+    for(let i = 0; i < nums.length; i++) {
+         for(let j = i + 1; j < nums.length; j++) {
+             if(nums[i] + nums[j] == target) {
+                 return [i, j]
+             }
+         }
+    }
+};
+
+
+console.log(twoSum([2,7,11,15], 9)) // [0,1]
+console.log(twoSum([3,2,4], 6)) // [1,2]
+console.log(twoSum([3,3], 6)) // [0,1]
+
